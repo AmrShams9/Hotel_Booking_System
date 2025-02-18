df091e5141bf1381157bbb9825e2acfc5429b68c
Amr Shams <amrkhaled104@gmail.com>
2025-02-18 20:15:38 +0200

    version

--- FrontEnd/HotelManagementSystem/src/App.jsx
+++ FrontEnd/HotelManagementSystem/src/App.jsx
@@ -6,34 +6,20 @@ import Login from './components/Login'
 import Signup from './components/SignUp'
 import LandingPage from './components/LandinPage'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-<<<<<<< HEAD
-=======
-
->>>>>>> bb71c7035811171d18d331cb2d01fb040d31a46d
 function App() {
 
 
   return (
-<<<<<<< HEAD
-
-=======
->>>>>>> bb71c7035811171d18d331cb2d01fb040d31a46d
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage></LandingPage>}></Route>
         <Route path="/login" element={<Login></Login>}></Route>
-<<<<<<< HEAD
         <Route path="/signup" element={<Signup></Signup>}></Route>
 
 
       </Routes>
     </Router >
 
-=======
-        <Route path="/register" element={<Signup></Signup>}></Route>
-      </Routes>
-    </Router>
->>>>>>> bb71c7035811171d18d331cb2d01fb040d31a46d
   );
 }
 
