29431af6859c430adf78a2d3084fd32a9da66df8
Amr Shams <amrkhaled104@gmail.com>
2025-03-12 04:24:35 +0200

    userconroller

--- BackEnd/userController.js
+++ BackEnd/userController.js
@@ -43,7 +43,7 @@ app.post("/register", async (req, res) => {
     id: mockUsers.length + 1,
     name,
     email,
-    password, // Store hashed password in a real app
+    password, 
     userType,
   };
 
@@ -82,6 +82,41 @@ app.get("/users", (req, res) => {
   
     res.status(200).json({ message: "Users retrieved successfully", users: usersWithoutPasswords });
   });
+  //Admin privilage
+  app.delete("/deleteuser" , (req,res) => {
+    const {email} = req.body;
+    const userIndex = mockUsers.findIndex((user) => user.email === email); // 0
+    const user  = mockUsers.find((user) => user.email === email);
+    if (userIndex !== -1) {
+       mockUsers.splice(user,1);
+       res.status(200).json({ message: "user deleted succssfully"});
+    }
+    else{
+      res.status(401).json({ message: "user doesn't exist"})
+
+    }
+
+  });
+  app.put("/updatepass",(req,res) =>{
+    const{email,password} = req.body;
+    const user  = mockUsers.find((user) => user.email === email);
+    if(user.password ===password){
+      res.status(401).json({ message: "choose a new password"});
+
+    }else{
+      user.password = password;
+      res.status(200).json({ message: "password changed successfully"});
+
+    }
+
+
+    
+
+    
+
+  });
+
+
 
 // Start the server
 app.listen(port, () => {
