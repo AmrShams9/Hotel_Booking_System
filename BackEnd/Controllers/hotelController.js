54fcebd1e2b4949c56b30f496154d5b86e83f797
Amr Shams <amrkhaled104@gmail.com>
2025-04-18 02:55:28 +0200

    hj

--- BackEnd/Controllers/hotelController.js
+++ BackEnd/Controllers/hotelController.js
@@ -7,7 +7,7 @@ router.post('/createHotel', async (req, res) => {
   try {
     const hotel = new Hotel(req.body); // ✅ استخدم اسم متغير مختلف
     await hotel.save();
-    res.status(201).json(hotel);
+    res.status(201).json({message: 'Success' ,hotel});
   } catch (error) {
     res.status(400).json({ message: 'Error creating hotel', error: error.message });
   }
@@ -22,5 +22,29 @@ router.get('/hotels', async (req, res) => {
     res.status(500).json({ message: 'Error retrieving hotels', error: error.message });
   }
 });
+
+//delete hotel
+router.delete('/hotels/:name', async (req, res) => {
+    const hotelName = req.params.name;
+  
+    try {
+      const deletedHotel = await Hotel.findOneAndDelete({ name: hotelName });
+  
+      if (!deletedHotel) {``
+        return res.status(404).json({ message: 'Hotel not found with this name' });
+      }
+  
+      res.status(200).json({ message: 'Hotel deleted successfully', hotel: deletedHotel });
+    } catch (error) {
+      res.status(500).json({ message: 'Error deleting hotel', error: error.message });
+    }
+  });
+
+
+
+
+//search hotel
+
+
 
 module.exports = router; // ✅ ضروري جدًا
